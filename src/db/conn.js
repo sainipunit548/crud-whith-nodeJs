7047024625baefed4ebd4cf4035f1e8d0a6b09b3
Punit <Punit>
2022-09-28 14:52:22 +0530

    change file

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/Student")
+const url = process.env.user ? `mongodb+srv://${process.env.user}:${process.env.password}@cluster0.3jrrbql.mongodb.net/?retryWrites=true&w=majority` : "mongodb://localhost:27017/Student"
+
+mongoose.connect(url)
     .then(() => {
         console.log("Connection is Successfully");
     }).catch((err) => {
